9196e06872d5f4540861aaa9eb2f20b7706e22c6
andrecosta9 <andre.vilhena.costa@gmail.com>
2023-03-11 17:22:37 +0000

    Adicionei o comando pip install roboflow dentro do test_andre.py

--- test_andre.py
+++ test_andre.py
@@ -1,9 +1,17 @@
-from roboflow import Roboflow
-#rf = Roboflow(api_key="ErF6qJjtrqfi7PykcPpj")
-#project = rf.workspace("conedetection").project("tidycity-iorn6")
-#dataset = project.version(1).download("yolov7")
+#Este código serve para fazer download do dataset, que inclui o TACO e um dataset de graffitis (https://zenodo.org/record/3238357#.ZAcVCXbP23D)
 
-rf = Roboflow(api_key="ErF6qJjtrqfi7PykcPpj")
-project = rf.workspace("alpr-yrtzl").project("alpr_new")
-dataset = project.version(1).download("yolov7")
+#Ao correr, usando python3 test_andre.py, uma pasta chamada TACO_Graffiti-2 vai ser criada
+#e é aí que vai ser guardado o dataset (que está no formato YOLOv7 pytorch).
+
+import os
+
+os.system("pip install roboflow")
+
+
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="0zi6trxO4J2tlDhqx04T")
+project = rf.workspace("feup-ohds6").project("taco_graffiti")
+dataset = project.version(2).download("yolov7")
+
 
